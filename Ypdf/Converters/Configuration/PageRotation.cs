fb34a3aa274e12d7528dbe38028f98e8d8da1c79
yakovypg <yasha-pmp@mail.ru>
2022-07-07 16:35:21 +0300

    Added Eraser, added Rotator

---
+++ Ypdf/Converters/Configuration/PageRotation.cs
@@ -0,0 +1,14 @@
+﻿namespace Ypdf.Converters.Configuration
+{
+    public struct PageRotation
+    {
+        public int PageNumber { get; set; }
+        public int Angle { get; set; }
+
+        public PageRotation(int pageNumber, int angle)
+        {
+            PageNumber = pageNumber;
+            Angle = angle;
+        }
+    }
+}
---
+++ Ypdf/Converters/Rotator.cs
@@ -0,0 +1,42 @@
+﻿using iText.Kernel.Pdf;
+using Ypdf.Converters.Configuration;
+
+namespace Ypdf.Converters
+{
+    public static class Rotator
+    {
+        public static void Rotate(string destPath, string inputPath, int angle)
+        {
+            var inputDoc = new PdfDocument(new PdfReader(inputPath));
+            var destDoc = new PdfDocument(new PdfWriter(destPath));
+
+            int pages = inputDoc.GetNumberOfPages();
+
+            for (int i = 1; i <= pages; ++i)
+                inputDoc.GetPage(i).SetRotation(-angle);
+
+            inputDoc.CopyPagesTo(1, pages, destDoc);
+
+            inputDoc.Close();
+            destDoc.Close();
+        }
+
+        public static void Rotate(string destPath, string inputPath, params PageRotation[] rotations)
+        {
+            if (rotations is null)
+                throw new ArgumentNullException(nameof(rotations));
+
+            var inputDoc = new PdfDocument(new PdfReader(inputPath));
+            var destDoc = new PdfDocument(new PdfWriter(destPath));
+
+            foreach (var rotation in rotations)
+                inputDoc.GetPage(rotation.PageNumber).SetRotation(-rotation.Angle);
+
+            int pages = inputDoc.GetNumberOfPages();
+            inputDoc.CopyPagesTo(1, pages, destDoc);
+
+            inputDoc.Close();
+            destDoc.Close();
+        }
+    }
+}
