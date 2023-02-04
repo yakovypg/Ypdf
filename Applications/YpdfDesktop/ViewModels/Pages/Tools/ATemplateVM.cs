8a6aab46225fc17bafd823e34410d0af6872be7e
yakovypg <yasha-pmp@mail.ru>
2023-02-04 19:50:16 +0300

    Add template for creating view models for tool views

---
+++ Applications/YpdfDesktop/ViewModels/Pages/Tools/ATemplateVM.cs
@@ -0,0 +1,42 @@
+﻿using YpdfDesktop.ViewModels.Base;
+
+namespace YpdfDesktop.ViewModels.Pages.Tools
+{
+    public class ATemplateVM : PdfToolViewModel
+    {
+        #region Commands
+
+        #endregion
+
+        #region Reactive Properties
+
+        #endregion
+
+        // Constructor for Designer
+        public ATemplateVM() : this(new SettingsViewModel(), new TasksViewModel())
+        {
+        }
+
+        public ATemplateVM(SettingsViewModel settingsVM, TasksViewModel tasksVM) : base(settingsVM, tasksVM)
+        {
+        }
+
+        #region Protected Methods
+
+        protected override void Execute()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void Reset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+    }
+}
