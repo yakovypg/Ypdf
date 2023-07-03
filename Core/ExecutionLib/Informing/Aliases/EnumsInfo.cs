3f9f9dc6531043b4586c3e142e5a706cf7eee000
yakovypg <yasha-pmp@mail.ru>
2023-07-03 11:59:51 +0300

    Add properties for getting collections of enum values

--- Core/ExecutionLib/Informing/Aliases/EnumsInfo.cs
+++ Core/ExecutionLib/Informing/Aliases/EnumsInfo.cs
@@ -1,4 +1,5 @@
 ﻿using iText.Layout.Properties;
+using System.Collections.Generic;
 using YpdfLib.Models.Design;
 
 namespace ExecutionLib.Informing.Aliases
@@ -10,5 +11,11 @@ namespace ExecutionLib.Informing.Aliases
         public static string TabAlignmentValues => string.Join('/', Enum.GetValues<TabAlignment>().Select(t => t.ToString().ToLower()));
         public static string HorizontalAlignmentValues => string.Join('/', Enum.GetValues<HorizontalAlignment>().Select(t => t.ToString().ToLower()));
         public static string VerticalAlignmentValues => string.Join('/', Enum.GetValues<VerticalAlignment>().Select(t => t.ToString().ToLower()));
+
+        public static IReadOnlyList<LocationMode> LocationModes => Enum.GetValues<LocationMode>().ToList();
+        public static IReadOnlyList<TextAlignment> TextAlignments => Enum.GetValues<TextAlignment>().ToList();
+        public static IReadOnlyList<TabAlignment> TabAlignments => Enum.GetValues<TabAlignment>().ToList();
+        public static IReadOnlyList<HorizontalAlignment> HorizontalAlignments => Enum.GetValues<HorizontalAlignment>().ToList();
+        public static IReadOnlyList<VerticalAlignment> VerticalAlignments => Enum.GetValues<VerticalAlignment>().ToList();
     }
 }
