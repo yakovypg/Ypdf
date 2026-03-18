dbab3719c9812cacdb5089d641edd2447e163c17
yakovypg <yasha-pmp@mail.ru>
2026-03-18 19:25:20 +0300

    Fix `CompressImageTool` creator

--- Applications/Ypdf.CommandLine/Creators/SupportedToolsCreator.cs
+++ Applications/Ypdf.CommandLine/Creators/SupportedToolsCreator.cs
@@ -14,7 +14,7 @@ internal sealed class SupportedToolsCreator : ISupportedToolsCreator
             { AddPageNumbersSubcommand.Name, new AddPageNumbersToolCreator() },
             { AddWatermarkAnnotationSubcommand.Name, new AddWatermarkAnnotationToolCreator() },
             { AddWatermarkSubcommand.Name, new AddWatermarkToolCreator() },
-            { CompressImagesSubcommand.Name, new CompressImagesToolCreator() },
+            { CompressImagesSubcommand.Name, new CompressImageToolCreator() },
             /*{ CompressSubcommand.Name, new CompressToolCreator() },
             { ConfigSubcommand.Name, new ConfigToolCreator() },
             { CopySubcommand.Name, new CopyToolCreator() },
---
+++ Applications/Ypdf.CommandLine/Creators/Tools/CompressImageToolCreator.cs
@@ -0,0 +1,39 @@
+using Ypdf.CommandLine.AppConfig;
+using Ypdf.CommandLine.Configuration;
+using Ypdf.CommandLine.Execution;
+using Ypdf.Core.Imaging;
+using Ypdf.Core.Runtime.Logging;
+using Ypdf.Core.Tools;
+
+namespace Ypdf.CommandLine.Creators.Tools;
+
+internal sealed class CompressImageToolCreator : IToolCreator
+{
+    public IToolExecutionProvider Create(YpdfParserConfig config)
+    {
+        ExtendedArgumentNullException.ThrowIfNull(config, nameof(config));
+
+        CompressImagesSubcommand subcommand = config.CompressImagesSubcommand;
+
+        var imageCompression = subcommand.Width is not null && subcommand.Height is not null
+            ? new ImageCompression(
+                subcommand.QualityFactor,
+                subcommand.Width.Value,
+                subcommand.Height.Value,
+                subcommand.Extension)
+            : new ImageCompression(
+                subcommand.QualityFactor,
+                subcommand.SizeFactor,
+                subcommand.Extension);
+
+        string? pythonAlias = GlobalConfig.Instance.PythonAlias;
+        IOutputWriter outputWriter = GlobalConfig.Instance.OutputWriter;
+
+        var tool = new CompressImageTool(imageCompression, pythonAlias, outputWriter);
+
+        return new ToolExecutionProvider(
+            tool,
+            subcommand.InputPaths,
+            subcommand.OutputPath);
+    }
+}
