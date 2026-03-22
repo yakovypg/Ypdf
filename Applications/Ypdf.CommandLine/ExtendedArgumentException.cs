415644da7dc1ca39afdd34da7321153d611f1f95
yakovypg <yasha-pmp@mail.ru>
2026-03-22 17:50:33 +0300

    Make inner exceptions internal

--- Applications/Ypdf.CommandLine/ExtendedArgumentException.cs
+++ Applications/Ypdf.CommandLine/ExtendedArgumentException.cs
@@ -4,7 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Serialization;
 
-namespace Ypdf.Core;
+namespace Ypdf.CommandLine;
 
 [Serializable]
 internal sealed class ExtendedArgumentException : ArgumentException
--- Core/Ypdf.Core/ExtendedArgumentNullException.cs
+++ Core/Ypdf.Core/ExtendedArgumentNullException.cs
@@ -5,31 +5,25 @@ using System.Runtime.Serialization;
 namespace Ypdf.Core;
 
 [Serializable]
-public class ExtendedArgumentNullException : ArgumentNullException
+internal sealed class ExtendedArgumentNullException : ArgumentNullException
 {
-    public ExtendedArgumentNullException() { }
+    internal ExtendedArgumentNullException() { }
 
-    public ExtendedArgumentNullException(string? paramName)
+    internal ExtendedArgumentNullException(string? paramName)
         : base(paramName) { }
 
-    public ExtendedArgumentNullException(string? message, Exception? innerException)
+    internal ExtendedArgumentNullException(string? message, Exception? innerException)
         : base(message, innerException) { }
 
-    public ExtendedArgumentNullException(string? paramName, string? message)
+    internal ExtendedArgumentNullException(string? paramName, string? message)
         : base(paramName, message) { }
 
 #if NET8_0_OR_GREATER
     [EditorBrowsable(EditorBrowsableState.Never)]
     [Obsolete("This API supports obsolete formatter-based serialization. It should not be called or extended by application code.", DiagnosticId = "SYSLIB0051", UrlFormat = "https://aka.ms/dotnet-warnings/{0}")]
 #endif
-    protected ExtendedArgumentNullException(SerializationInfo info, StreamingContext context)
+    private ExtendedArgumentNullException(SerializationInfo info, StreamingContext context)
         : base(info, context) { }
-
-    public static void ThrowIfNull<T>(T? argument, string? paramName = null)
-    {
-        if (argument is null)
-            throw new ArgumentNullException(paramName);
-    }
 
 #if NET8_0_OR_GREATER
     [EditorBrowsable(EditorBrowsableState.Never)]
@@ -39,5 +33,11 @@ public class ExtendedArgumentNullException : ArgumentNullException
     {
         ThrowIfNull(info, nameof(info));
         base.GetObjectData(info, context);
+    }
+
+    internal static void ThrowIfNull<T>(T? argument, string? paramName = null)
+    {
+        if (argument is null)
+            throw new ArgumentNullException(paramName);
     }
 }
