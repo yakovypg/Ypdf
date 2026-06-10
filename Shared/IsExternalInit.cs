bd721ecf10f50697c7c64f41747c44e40d58179c
yakovypg <yasha-pmp@mail.ru>
2026-06-10 23:30:32 +0300

    Remove unused files

--- Shared/IsExternalInit.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel;
-
-namespace System.Runtime.CompilerServices;
-
-#pragma warning disable CA1812 // Avoid uninstantiated internal classes
-[EditorBrowsable(EditorBrowsableState.Never)]
-internal sealed class IsExternalInit { }
-#pragma warning restore CA1812 // Avoid uninstantiated internal classes
